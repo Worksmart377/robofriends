7b130a793ec25cb3cde47356668e59d397a805a6
Mrs_N <worksmart377@gmail.com>
2023-06-16 16:30:27 -0500

    creates searchbar component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import 'tachyons';
 import CardList from './CardList';
 import { robots } from './robots';
+import SearchBar from './SearchBar';
 
 function App() {
     return (
-        <div className="App">
+        <div className="App tc">
         <header className="App-header">
-                <div>
-                    <CardList robots={robots} />
-                </div>
+        <h1>RoboFriends</h1>   
+        <SearchBar />    
+        <CardList robots={robots} />
+
         </header>
         </div>
     );
