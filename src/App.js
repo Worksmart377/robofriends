2b0d10f08c3bbbf1cfcd0f2f2ae8381b4f8dc912
Mrs_N <worksmart377@gmail.com>
2023-06-16 16:13:56 -0500

    creates card component for robots

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import './App.css';
+import React from 'react';
+import 'tachyons';
+import CardList from './CardList';
+import { robots } from './robots';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+        <header className="App-header">
+                <div>
+                    <CardList robots={robots} />
+                </div>
+        </header>
+        </div>
+    );
 }
 
 export default App;
